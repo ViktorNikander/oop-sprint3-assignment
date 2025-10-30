e111aa0900ac4747d5844e1eaa2e772d237ec74d
Viktor Nikander <vikvil91@gmail.com>
2025-10-30 10:01:19 +0100

    add isMoveable logic

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -16,6 +16,7 @@ public class FifteenGame extends JFrame implements ActionListener {
     boolean win = false;
     JButton buttonPressed;
     int indexOfEmptyLabel = 16;
+    int[] validTilesForMoving = new int[4];
 
     FifteenGame(){
         add(base);
@@ -57,19 +58,146 @@ public class FifteenGame extends JFrame implements ActionListener {
             indexOfEmptyLabel = labelList.indexOf(labelContainingButton);
         }
         System.out.println(indexOfEmptyLabel);
+        System.out.println(isMoveable(labelContainingButton));
         repaint();
     }
 
-    private void moveTiles(JButton buttonPressed){
+    private void moveTiles(JLabel labelContainingButton){
         if (true){ //TODO make true method for checking empty and pressed tile are next to each other
 
         }
     }
 
-    private boolean isMoveable(JButton buttonPressed){
-        int[] validTilesForMoving; //TODO add method for returning valid tiles based on empty label
+    private boolean isMoveable(JLabel labelContainingButton){
+        validTilesForMoving();
+        int tileWantingToMove = labelList.indexOf(labelContainingButton);
+        System.out.println(tileWantingToMove + " tile number wanting to move");
+        for (int number : validTilesForMoving){
+            if (tileWantingToMove == number){
+                return true;
+            }
+        }
+        return false;
+    }
 
-        return true;
+    private int[] validTilesForMoving() {
+        switch (indexOfEmptyLabel){
+            case 1 -> {
+                validTilesForMoving[0] = 0;
+                validTilesForMoving[1] = 5;
+                validTilesForMoving[2] = 2;
+                validTilesForMoving[3] = 0;
+                return validTilesForMoving;
+            }
+            case 2 -> {
+                validTilesForMoving[0] = 1;
+                validTilesForMoving[1] = 6;
+                validTilesForMoving[2] = 3;
+                validTilesForMoving[3] = 0;
+                return validTilesForMoving;
+            }
+            case 3 -> {
+                validTilesForMoving[0] = 2;
+                validTilesForMoving[1] = 7;
+                validTilesForMoving[2] = 4;
+                validTilesForMoving[3] = 0;
+                return validTilesForMoving;
+            }
+            case 4 -> {
+                validTilesForMoving[0] = 3;
+                validTilesForMoving[1] = 8;
+                validTilesForMoving[2] = 0;
+                validTilesForMoving[3] = 0;
+                return validTilesForMoving;
+            }
+            case 5 -> {
+                validTilesForMoving[0] = 0;
+                validTilesForMoving[1] = 9;
+                validTilesForMoving[2] = 6;
+                validTilesForMoving[3] = 1;
+                return validTilesForMoving;
+            }
+            case 6 -> {
+                validTilesForMoving[0] = 5;
+                validTilesForMoving[1] = 10;
+                validTilesForMoving[2] = 7;
+                validTilesForMoving[3] = 2;
+                return validTilesForMoving;
+            }
+            case 7 -> {
+                validTilesForMoving[0] = 6;
+                validTilesForMoving[1] = 11;
+                validTilesForMoving[2] = 8;
+                validTilesForMoving[3] = 3;
+                return validTilesForMoving;
+            }
+            case 8 -> {
+                validTilesForMoving[0] = 7;
+                validTilesForMoving[1] = 12;
+                validTilesForMoving[2] = 0;
+                validTilesForMoving[3] = 4;
+                return validTilesForMoving;
+            }
+            case 9 -> {
+                validTilesForMoving[0] = 0;
+                validTilesForMoving[1] = 13;
+                validTilesForMoving[2] = 10;
+                validTilesForMoving[3] = 5;
+                return validTilesForMoving;
+            }
+            case 10 -> {
+                validTilesForMoving[0] = 9;
+                validTilesForMoving[1] = 14;
+                validTilesForMoving[2] = 11;
+                validTilesForMoving[3] = 6;
+                return validTilesForMoving;
+            }
+            case 11 -> {
+                validTilesForMoving[0] = 10;
+                validTilesForMoving[1] = 15;
+                validTilesForMoving[2] = 12;
+                validTilesForMoving[3] = 7;
+                return validTilesForMoving;
+            }
+            case 12 -> {
+                validTilesForMoving[0] = 11;
+                validTilesForMoving[1] = 16;
+                validTilesForMoving[2] = 0;
+                validTilesForMoving[3] = 8;
+                return validTilesForMoving;
+            }
+            case 13 -> {
+                validTilesForMoving[0] = 0;
+                validTilesForMoving[1] = 0;
+                validTilesForMoving[2] = 14;
+                validTilesForMoving[3] = 9;
+                return validTilesForMoving;
+            }
+            case 14 -> {
+                validTilesForMoving[0] = 13;
+                validTilesForMoving[1] = 0;
+                validTilesForMoving[2] = 15;
+                validTilesForMoving[3] = 10;
+                return validTilesForMoving;
+            }
+            case 15 -> {
+                validTilesForMoving[0] = 14;
+                validTilesForMoving[1] = 0;
+                validTilesForMoving[2] = 16;
+                validTilesForMoving[3] = 11;
+                return validTilesForMoving;
+            }
+            case 16 -> {
+                validTilesForMoving[0] = 15;
+                validTilesForMoving[1] = 0;
+                validTilesForMoving[2] = 0;
+                validTilesForMoving[3] = 12;
+                return validTilesForMoving;
+            }
+            default -> {
+                return null;
+            }
+        }
     }
 
     private JButton createButton(String text){
