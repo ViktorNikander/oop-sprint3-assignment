a9de47556b1b0191ff96bb1248b03c90cd10d213
Viktor Nikander <vikvil91@gmail.com>
2025-10-31 17:02:48 +0100

    refactor valid tiles for moving

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -17,7 +17,6 @@ public class FifteenGame extends JFrame implements ActionListener {
     boolean win = false;
     JButton buttonPressed;
     int indexOfEmptyLabel = 16;
-    int[] validTilesForMoving = new int[4];
     FifteenGame(){
         add(base);
         base.add(header, BorderLayout.NORTH);
@@ -46,6 +45,7 @@ public class FifteenGame extends JFrame implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        buttonPressed = (JButton) e.getSource();
         if (e.getSource() == newGame){
             createNewGame();
         }else {
@@ -95,132 +95,18 @@ public class FifteenGame extends JFrame implements ActionListener {
         }
     }
     private boolean isMoveable(JLabel labelContainingButton){
-        validTilesForMoving();
-        int tileWantingToMove = labelList.indexOf(labelContainingButton);
-        for (int number : validTilesForMoving){
-            if (tileWantingToMove == number){
-                return true;
-            }
-        }
-        return false;
-    }
-    private int[] validTilesForMoving() {
-        switch (indexOfEmptyLabel){
-            case 1 -> {
-                validTilesForMoving[0] = 0;
-                validTilesForMoving[1] = 5;
-                validTilesForMoving[2] = 2;
-                validTilesForMoving[3] = 0;
-                return validTilesForMoving;
-            }
-            case 2 -> {
-                validTilesForMoving[0] = 1;
-                validTilesForMoving[1] = 6;
-                validTilesForMoving[2] = 3;
-                validTilesForMoving[3] = 0;
-                return validTilesForMoving;
-            }
-            case 3 -> {
-                validTilesForMoving[0] = 2;
-                validTilesForMoving[1] = 7;
-                validTilesForMoving[2] = 4;
-                validTilesForMoving[3] = 0;
-                return validTilesForMoving;
-            }
-            case 4 -> {
-                validTilesForMoving[0] = 3;
-                validTilesForMoving[1] = 8;
-                validTilesForMoving[2] = 0;
-                validTilesForMoving[3] = 0;
-                return validTilesForMoving;
-            }
-            case 5 -> {
-                validTilesForMoving[0] = 0;
-                validTilesForMoving[1] = 9;
-                validTilesForMoving[2] = 6;
-                validTilesForMoving[3] = 1;
-                return validTilesForMoving;
-            }
-            case 6 -> {
-                validTilesForMoving[0] = 5;
-                validTilesForMoving[1] = 10;
-                validTilesForMoving[2] = 7;
-                validTilesForMoving[3] = 2;
-                return validTilesForMoving;
-            }
-            case 7 -> {
-                validTilesForMoving[0] = 6;
-                validTilesForMoving[1] = 11;
-                validTilesForMoving[2] = 8;
-                validTilesForMoving[3] = 3;
-                return validTilesForMoving;
-            }
-            case 8 -> {
-                validTilesForMoving[0] = 7;
-                validTilesForMoving[1] = 12;
-                validTilesForMoving[2] = 0;
-                validTilesForMoving[3] = 4;
-                return validTilesForMoving;
-            }
-            case 9 -> {
-                validTilesForMoving[0] = 0;
-                validTilesForMoving[1] = 13;
-                validTilesForMoving[2] = 10;
-                validTilesForMoving[3] = 5;
-                return validTilesForMoving;
-            }
-            case 10 -> {
-                validTilesForMoving[0] = 9;
-                validTilesForMoving[1] = 14;
-                validTilesForMoving[2] = 11;
-                validTilesForMoving[3] = 6;
-                return validTilesForMoving;
-            }
-            case 11 -> {
-                validTilesForMoving[0] = 10;
-                validTilesForMoving[1] = 15;
-                validTilesForMoving[2] = 12;
-                validTilesForMoving[3] = 7;
-                return validTilesForMoving;
-            }
-            case 12 -> {
-                validTilesForMoving[0] = 11;
-                validTilesForMoving[1] = 16;
-                validTilesForMoving[2] = 0;
-                validTilesForMoving[3] = 8;
-                return validTilesForMoving;
-            }
-            case 13 -> {
-                validTilesForMoving[0] = 0;
-                validTilesForMoving[1] = 0;
-                validTilesForMoving[2] = 14;
-                validTilesForMoving[3] = 9;
-                return validTilesForMoving;
-            }
-            case 14 -> {
-                validTilesForMoving[0] = 13;
-                validTilesForMoving[1] = 0;
-                validTilesForMoving[2] = 15;
-                validTilesForMoving[3] = 10;
-                return validTilesForMoving;
-            }
-            case 15 -> {
-                validTilesForMoving[0] = 14;
-                validTilesForMoving[1] = 0;
-                validTilesForMoving[2] = 16;
-                validTilesForMoving[3] = 11;
-                return validTilesForMoving;
-            }
-            case 16 -> {
-                validTilesForMoving[0] = 15;
-                validTilesForMoving[1] = 0;
-                validTilesForMoving[2] = 0;
-                validTilesForMoving[3] = 12;
-                return validTilesForMoving;
-            }
-            default -> {
-                return null;
-            }
+        Rectangle emptySpace = labelList.get(indexOfEmptyLabel).getBounds();
+        Rectangle pressedSpace = labelContainingButton.getBounds();
+        if ((pressedSpace.getX() + pressedSpace.getWidth() == emptySpace.getX()) && pressedSpace.getY() == emptySpace.getY()) {
+            return true;
+        } else if (pressedSpace.getY() + pressedSpace.getHeight() == emptySpace.getY() && pressedSpace.getX() == emptySpace.getX()) {
+            return true;
+        } else if (pressedSpace.getX() == emptySpace.getX() + emptySpace.getWidth() && pressedSpace.getY() == emptySpace.getY()) {
+            return true;
+        } else if (pressedSpace.getY() == emptySpace.getY() + emptySpace.getHeight() && pressedSpace.getX() == emptySpace.getX()) {
+            return true;
+        } else {
+            return false;
         }
     }
     private JButton createButton(String text){
