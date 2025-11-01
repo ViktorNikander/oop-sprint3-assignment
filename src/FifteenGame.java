62b882086751f8eeb3d85293b80664dc6fceadd9
Viktor Nikander <vikvil91@gmail.com>
2025-11-01 11:07:29 +0100

    remove tiles, need to fix methods

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -13,10 +13,10 @@ public class FifteenGame extends JFrame implements ActionListener {
     JButton newGame = new JButton("New game");
     JLabel winLabel = new JLabel();
     List<JButton> buttonList = new ArrayList<>();
-    List<JLabel> labelList = new ArrayList<>();
+    JButton emptyButton = new JButton();
     boolean win = false;
     JButton buttonPressed;
-    int indexOfEmptyLabel = 16;
+
     FifteenGame(){
         add(base);
         base.add(header, BorderLayout.NORTH);
@@ -24,19 +24,18 @@ public class FifteenGame extends JFrame implements ActionListener {
         header.add(newGame);
         newGame.addActionListener(this);
         header.add(winLabel);
-        labelList.add(createLabel());
-        for (int i = 1; i <= 16; i++) {
-            labelList.add(createLabel());
-            body.add(labelList.get(i));
-        }
-        buttonList.add(createButton(""));
+
         for (int i = 1; i < 16; i++) {
             buttonList.add(createButton(String.valueOf(i)));
         }
-        Collections.shuffle(buttonList.subList(1, buttonList.size())); //Comment out to create a winning game
-        for (int i = 1; i < 16; i++) {
-            labelList.get(i).add(buttonList.get(i));
+        buttonList.add(emptyButton);
+        Collections.shuffle(buttonList); //Comment out to create a winning game
+        for (JButton button : buttonList){
+            body.add(button);
+            if (button.getText() == null){
+            }
         }
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -50,65 +49,52 @@ public class FifteenGame extends JFrame implements ActionListener {
             createNewGame();
         }else {
             buttonPressed = (JButton) e.getSource();
-            JLabel labelContainingButton = (JLabel) buttonPressed.getParent();
-            moveTiles(labelContainingButton, buttonPressed);
+//            moveTiles(buttonPressed);
             checkWin();
         }
     }
     private void createNewGame(){
-        for (JButton button : buttonList.subList(1, buttonList.size())){
-            JLabel label = (JLabel) button.getParent();
-            label.remove(button);
-            Collections.shuffle(buttonList.subList(1, buttonList.size()));
-            for (int i = 1; i < 16; i++) {
-                labelList.get(i).add(buttonList.get(i));
-            }
+        body.removeAll();
+        Collections.shuffle(buttonList);
+        for (JButton button : buttonList){
+            body.add(button);
         }
         revalidate();
         repaint();
     }
     private void checkWin() {
-        for (JButton button : buttonList){
-            if (buttonList.indexOf(button) == 0){ //Skips index 0 of list
-                continue;
-            }
-            win = true;
-            int placeholder = Integer.parseInt(button.getText());
-            if (placeholder != labelList.indexOf(button.getParent())){
-                win = false;
-                break;
+        JButton testedButton;
+        for (int i = 1; i < 16; i++) {
+            testedButton = (JButton) body.getComponent(i - 1);
+            if (Integer.parseInt(testedButton.getText()) != i){
+                winLabel.setText("");
+                return;
             }
         }
-        if (win){
-            winLabel.setText("You won!");
-        }else {
-            winLabel.setText("");
-        }
+        winLabel.setText("You win");
     }
-    private void moveTiles(JLabel labelContainingButton, JButton buttonPressed){
-        if (isMoveable(labelContainingButton)){
-            labelList.get(indexOfEmptyLabel).add(buttonPressed);
-            labelContainingButton.remove(buttonPressed);
-            indexOfEmptyLabel = labelList.indexOf(labelContainingButton);
-            revalidate();
-            repaint();
-        }
-    }
-    private boolean isMoveable(JLabel labelContainingButton){
-        Rectangle emptySpace = labelList.get(indexOfEmptyLabel).getBounds();
-        Rectangle pressedSpace = labelContainingButton.getBounds();
-        if ((pressedSpace.getX() + pressedSpace.getWidth() == emptySpace.getX()) && pressedSpace.getY() == emptySpace.getY()) {
-            return true;
-        } else if (pressedSpace.getY() + pressedSpace.getHeight() == emptySpace.getY() && pressedSpace.getX() == emptySpace.getX()) {
-            return true;
-        } else if (pressedSpace.getX() == emptySpace.getX() + emptySpace.getWidth() && pressedSpace.getY() == emptySpace.getY()) {
-            return true;
-        } else if (pressedSpace.getY() == emptySpace.getY() + emptySpace.getHeight() && pressedSpace.getX() == emptySpace.getX()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+//    private void moveTiles(JButton buttonPressed){
+//        if (isMoveable(buttonPressed)){
+//            System.out.println("can move");
+//            revalidate();
+//            repaint();
+//        }
+//    }
+//    private boolean isMoveable(JButton buttonPressed){
+//        Rectangle emptySpace =
+//        Rectangle pressedSpace = labelContainingButton.getBounds();
+//        if ((pressedSpace.getX() + pressedSpace.getWidth() == emptySpace.getX()) && pressedSpace.getY() == emptySpace.getY()) {
+//            return true;
+//        } else if (pressedSpace.getY() + pressedSpace.getHeight() == emptySpace.getY() && pressedSpace.getX() == emptySpace.getX()) {
+//            return true;
+//        } else if (pressedSpace.getX() == emptySpace.getX() + emptySpace.getWidth() && pressedSpace.getY() == emptySpace.getY()) {
+//            return true;
+//        } else if (pressedSpace.getY() == emptySpace.getY() + emptySpace.getHeight() && pressedSpace.getX() == emptySpace.getX()) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
     private JButton createButton(String text){
         JButton button = new JButton(text);
         button.setBackground(Color.red);
@@ -117,10 +103,6 @@ public class FifteenGame extends JFrame implements ActionListener {
         button.setSize(100, 100);
         button.addActionListener(this);
         return button;
-    }
-    private JLabel createLabel(){
-        JLabel label = new JLabel();
-        return label;
     }
     public static void main(String[] args) {
         FifteenGame game = new FifteenGame();
